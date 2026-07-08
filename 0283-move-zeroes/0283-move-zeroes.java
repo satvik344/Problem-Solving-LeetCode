2dcce215eea385c7c25dd0e2fe48d0e1e0414480
SATVIK CHERUKURI <76486986+satvik344@users.noreply.github.com>
2026-07-09 02:15:41 +0530

    Time: 2 ms (92.2%), Space: 47.8 MB (56.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,22 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = 0;
-        for(int i = 0; i<nums.length;i++){
-            if(nums[i]!=0){
-                    int temp = nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                    j++;
+        if (nums.length == 1) {
+            return;
+        }
+
+        int nonZeroIndex = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex] = nums[i];
+                nonZeroIndex++;
             }
-            }
-           
+        }
+
+       
+         while(nonZeroIndex<nums.length){
+            nums[nonZeroIndex] = 0;
+            nonZeroIndex++;
         }
     }
+}
